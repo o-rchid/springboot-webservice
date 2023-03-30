79d7d6e9afa4b6fce844ff527878617b3c760778
o-rchid <ozzve35@gmail.com>
2023-03-30 23:45:11 +0900

    롬복 전환, ResponseDto 적용 및 TestCode

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -8,13 +8,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) //스프링 부트 테스트와 JUnit 사이 연결자
 @WebMvcTest(controllers = HelloController.class) //스프링 테스트 어노테이션 중 Web(Spring MVC)에 집중할 수 있는 어노테이션
-public class HelloControllerTest extends TestCase {
+public class HelloControllerTest{
 
     @Autowired
     private MockMvc mvc; //웹 API를 테스트할 때 사용, 스프링 MVC 테스트의 시작점
@@ -26,5 +26,20 @@ public class HelloControllerTest extends TestCase {
         mvc.perform(get("/hello")) // /hello 주소로 GET 요청을 함, 체이닝으로 여러 검증 가능
                 .andExpect(status().isOk()) //HTTP Header의 Status 검증
                 .andExpect(content().string(hello)); //응답 본문의 내용을 검증
+    }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount))
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
