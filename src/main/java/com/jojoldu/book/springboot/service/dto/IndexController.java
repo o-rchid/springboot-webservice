246bf328023016db5a7f1c44be7c6e91572a3257
o-rchid <ozzve35@gmail.com>
2023-08-31 23:28:03 +0900

    로그인 테스트

--- src/main/java/com/jojoldu/book/springboot/service/dto/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/service/dto/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.service.dto;
 
+import com.jojoldu.book.springboot.config.auth.SessionUser;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,16 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model)
     {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
