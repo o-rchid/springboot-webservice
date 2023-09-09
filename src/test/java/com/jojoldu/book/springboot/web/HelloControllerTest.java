774f875e2fabb844170ef5eac612aafb61a7c000
o-rchid <ozzve35@gmail.com>
2023-09-09 23:13:09 +0900

    Spring Security TestCode 오류 수정

--- src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.config.auth.SecurityConfig;
 import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,12 +17,16 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class) //스프링 부트 테스트와 JUnit 사이 연결자
-@WebMvcTest(controllers = HelloController.class) //스프링 테스트 어노테이션 중 Web(Spring MVC)에 집중할 수 있는 어노테이션
+@WebMvcTest(controllers = HelloController.class //스프링 테스트 어노테이션 중 Web(Spring MVC)에 집중할 수 있는 어노테이션
+  ,excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest{
 
     @Autowired
     private MockMvc mvc; //웹 API를 테스트할 때 사용, 스프링 MVC 테스트의 시작점
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -28,6 +36,7 @@ public class HelloControllerTest{
                 .andExpect(content().string(hello)); //응답 본문의 내용을 검증
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
